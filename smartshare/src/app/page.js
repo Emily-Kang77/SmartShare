513754c7b6d83be6b8d83f45367f6cb68a7f4260
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-10-04 13:14:08 -0700

    Update amplify config to use github instead of local code

--- smartshare/src/app/page.js
+++ smartshare/src/app/page.js
@@ -1,4 +1,5 @@
 // import Image from "next/image";
+import Link from 'next/link'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -6,9 +7,14 @@ export default function Home() {
   return (
     <ChakraProvider>
       <div>
-        Welcome to SmartShare.
 
-        Sign up / login 
+        <p>Welcome to SmartShare. </p>
+        
+
+        <ul>
+          <li className="to-blue-500"><Link href="/signin">Sign in</Link></li>
+          <li><Link href="/profile">Profile</Link></li>
+        </ul>
       </div>
     </ChakraProvider>
   );
